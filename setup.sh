724c4eeaecb96706aaf75196bf2e314404fb8000
capusta <capusta@users.noreply.github.com>
2017-06-23 07:07:44 +0000

    Configuring deluge client

--- setup.sh
+++ setup.sh
@@ -9,13 +9,28 @@ fi
 echo apt-get update
 echo apt-get install -y openvpn deluged deluge-console \
                         samba samba-common-bin
+deluged &
 
 VPN_CFG=/etc/openvpn/login.conf
-
 if [ -e $VPN_CFG ]; then
   echo 'Openvpn is configured - check username / password'
 else
   echo 'Setting up generic login credentials'
   echo 'username' > $VPN_CFG
   echo 'secretpass' >> $VPN_CFG
+  # TODO: insert login.conf stanza in all config files
+fi
+
+deluge-console config set allow_remote True
+### Deluge client configuration
+DLG_AUTH=/home/pi/.config/deluge/auth
+grep -q "^#setup-complete" $DLG_AUTH
+if [ $? -eq 1 ]; then
+  pkill deluged
+  echo 'Configuring deluge auth'
+  echo '#setup-complete' > $DLG_AUTH
+  echo 'pi:raspberry:10' >> $DLG_AUTH
+  deluged
+else
+  echo 'Deluge auth is configured'
 fi
