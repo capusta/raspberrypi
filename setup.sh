d797c934be178a671f250b99e9eb7ec0bab654a8
capusta <capusta@users.noreply.github.com>
2017-06-23 06:34:18 +0000

    Second commit

--- setup.sh
+++ setup.sh
@@ -7,3 +7,15 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 echo apt-get update
+echo apt-get install -y openvpn deluged deluge-console \
+                        samba samba-common-bin
+
+VPN_CFG=/etc/openvpn/login.conf
+
+if [ -e $VPN_CFG ]; then
+  echo 'Openvpn is configured - check username / password'
+else
+  echo 'Setting up generic login credentials'
+  echo 'username' > $VPN_CFG
+  echo 'secretpass' >> $VPN_CFG
+fi
