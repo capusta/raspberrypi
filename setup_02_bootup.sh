abf8a313e01c25448db05efa14cc7a070e0ba484
capusta <capusta@users.noreply.github.com>
2017-06-28 01:15:23 -0700

    refactor init setup

--- setup_02_bootup.sh
+++ setup_02_bootup.sh
@@ -1,13 +1,25 @@
 #! /bin/bash
 set -e
 
-function addVPN(){
+function prefix(){
+  head -n -1 /etc/rc.local > /etc/rc.local.bak
+}
+
+function suffix(){
+  mv -v /etc/rc.local.bak /etc/rc.local
+  echo 'exit 0' >> /etc/rc.local
   return
+}
+
+function addVPN(){
+  echo '/usr/sbin/openvpn --config Denmark.ovpn' >> /etc/rc.local.bak
 }
 
 function addDeluge(){
-  return
+  echo 'TODO'
 }
 
+prefix
 cat /etc/rc.local | grep -i openvpn || addVPN
 cat /etc/rc.local | grep -i deluge || addDeluge
+suffix
