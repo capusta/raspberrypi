96537390c573ab4fca0916aee749fcea96d6f4b0
capusta <capusta@users.noreply.github.com>
2017-07-02 00:49:23 -0700

    Finilizing cron

--- overwatch.sh
+++ overwatch.sh
@@ -3,13 +3,29 @@
 ### Let's keep track on deluge and openvpn, and make sure one
 ### Does not run without the other
 
+function log(){
+  logger -t overwatch $1
+}
+
 ps -ef | grep -qi "[o]penvpn --config" && OPENVPN=true
 ps -ef | grep -qi "[d]eluged" && DELUGE=true
 
 if [[ -z $OPENVPN ]]; then
-  CUR_IP=$(curl ipinfo.io/ip)
-  echo "Current IP is $CUR_IP ... starting openvpn"
-  #TODO: do some ip comparison here to make sure we got it
+  OLD_IP=$(curl -s ipinfo.io/ip)
+  log "Current IP is $OLD_IP ... starting openvpn"
+  pushd /etc/openvpn > /dev/null
+    openvpn --config /etc/openvpn/Denmark.ovpn &
+    STS=$!
+    sleep 6
+    ps -ef | grep "$STS" && OPENVPN=true
+  popd
+  NEW_IP=$(curl -s ipinfo.io/ip)
+  if [[ "$OLD_IP" == "$NEW_IP" ]]; then
+    log "ERROR: IP is not changing when using VPN, exiting"
+    exit 1 
+  else
+    log "SUCCESS: IP is set up"
+  fi
 fi
 
 if [[ -z $DELUGE ]]; then
@@ -17,29 +33,15 @@ if [[ -z $DELUGE ]]; then
   STS=$!
   sleep 3
   ps -ef | grep "$STS" && DELUGE=true
+  log "Started Deluge"
 fi
 
 
 if [[ -z $DELUGE || -z $OPENVPN ]]; then
-  echo 'Nothing is running ... exiting'
+  log 'EIther deluge or Openvpn not running, killing both and exiting'
+  sudo pkill deluged || true
+  sudo pkill openvpn || true
   exit 1
 fi
-echo 'All is good'
+log 'Overwatch complete'
 exit 0
-
-kill deluge (openvpn is bad)
-check ip
-start openvpn 
-  give some time
-
-if (openvpn is good)
-   if currentip == previous ip
-         log error, exit
-   else
-      start deluged
-
-confirm openvpn and deluged is running
-   if not -> log error
-   exit
-    
-      
