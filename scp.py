4de58614c3db10d32ca678d872809b352e399d83
Capusta <capusta@users.noreply.github.com>
2017-08-02 10:24:50 -0700

    Check credentials

--- scp.py
+++ scp.py
@@ -11,9 +11,13 @@ deluge_info_raw = {}
 
 def main():
   ''' Primary Script of corrolating files and removing known completed files from deluge '''
+  check_creds()
   check_call(['whoami'])
 
-  deluge_info_raw = ""
+  if clean_up:
+    os.remove(deluge_file)
+    log("Removed {0}".format(deluge_file))
+
   if os.path.isfile(deluge_file) and os.path.getsize(deluge_file) > 0:
     log('Using provided file: {0}'.format(deluge_file))
     with open(deluge_file,'r') as f:
@@ -38,9 +42,21 @@ def main():
       name = False
       continue
 
-  if clean_up:
-    os.remove(deluge_file)
-    log("Removed {0}".format(deluge_file))
+def check_creds():
+  c = os.path.isfile(scp_creds_file) and os.path.getsize(scp_creds_file) > 0
+  if not c:
+    raise IOError("ERROR: missing credentials file {0}".format(scp_creds_file))
+  with open(scp_creds_file,'r') as f:
+    for line in f:
+      try:
+        line = line.strip().split('=')
+        k = line[0]
+        v = line[1]
+        os.environ[k] = v
+        log("Setting {0} = {1}".format(k,os.environ[k]))
+      except:
+        log("Ignoring configuration item: {0}".format(line))
+        continue
 
 def check_output(command):
   return subprocess.check_output(command,universal_newlines=True)
